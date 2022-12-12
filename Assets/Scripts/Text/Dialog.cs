a5155583eb4401efec5e0aa16ce0249a6d7aa977
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-12-13 04:03:50 +0800

    dialogs added for level 1

---
+++ Assets/Scripts/Text/Dialog.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    //public Text speech;
+    public Text button;
+    public TextMeshProUGUI Text;
+    public GameObject speech;
+    public GameObject btn;
+    public GameObject dialog;
+
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Text = speech.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (index == 1) {
+            //修改btn文字
+            button.text = "Wow, thank you!";
+            //修改speech
+            Text.text = "Considering that you have been away from home for a long time. So I arranged some hints around the screen.";
+        }
+        
+        if (index == 2) {
+            //修改btn文字
+            button.text = "What can I do with it?";
+            //修改speech
+            Text.text = "Be careful, these wizards are cunning. They have arranged a lot of little monsters nearby.";
+            // But don't worry too much, I believe that with your strength, you can kill them with one blow.
+        }
+
+        if (index == 2) {
+            button.text = "But this hammer sucks.";
+            Text.text = "You don't need worry too much, I believe you can kill with one blow.";
+        }
+
+        if (index == 3) {
+            button.text = "That`s great!";
+            Text.text = "Oh my boy, watch out, there are magic learning points on the way. You can get the magic you need there.";
+        }
+
+        if (index == 3) {
+            Destroy(btn);
+            Text.text = "Come on boy, the future of our hometown depends on you.";
+            Invoke("des", 2f);
+        }
+    }
+
+    public void Next(){
+        index++;
+        Debug.Log("index = " + index);
+    }
+
+    public void des(){
+        Destroy(dialog);
+    }
+}
