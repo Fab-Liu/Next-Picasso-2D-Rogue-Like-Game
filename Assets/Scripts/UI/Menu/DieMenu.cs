421d939bb71766233da1944852046d5f0a1a747d
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2022-12-17 14:58:23 +0800

    die tip

--- Assets/Scripts/UI/Menu/DieMenu.cs
+++ Assets/Scripts/UI/Menu/DieMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 namespace Diemenu
 {
     public class DieMenu : MonoBehaviour
     {
         private GameObject total;
         private Button restart;
+        private
 
         void Start()
         {
@@ -26,9 +28,18 @@ namespace Diemenu
             GameObject dieMenu = GameObject.Find("Canvas (1)/DieMenu");
             GameObject total = dieMenu.transform.Find("Total").gameObject;
             total.SetActive(true);
-
+            ArrayList al = buildList();
+            total.transform.Find("Tip").GetComponent<TMP_Text>().text = "Tips:" + al[Random.Range(0, al.Count)];
             Button restart = total.transform.Find("Restart").GetComponent<Button>();
             restart.onClick.AddListener(() => { reloadScene(); });
+        }
+        static private ArrayList buildList()
+        {
+            ArrayList al = new ArrayList();
+            al.Add("Use you weapon to kill the enemy");
+            al.Add("Avoid the enemy's attack");
+            al.Add("Buy some goods in the store");
+            return al;
         }
         static void reloadScene()
         {
