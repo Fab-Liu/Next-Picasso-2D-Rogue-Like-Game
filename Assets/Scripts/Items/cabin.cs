0a1a47a9075dadd592ded9bbd7dad6bd43f4f5c6
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2022-12-18 12:42:38 +0800

    level 2 optimism

---
+++ Assets/Scripts/Items/cabin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cabin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        Debug.Log("Trigger");
+        if (collidedObject.gameObject.tag == "Player")
+        {
+            pass();
+        }
+    }
+    void pass()
+    {
+        SceneManager.LoadScene("L2_work");
+    }
+}
